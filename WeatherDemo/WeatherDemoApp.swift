655ea42bf4e02f8bb462b907c7e343d2baf4fc02
Liyou <liyouleo911@msn.com>
2021-12-10 14:09:46 +0800

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Liyou on 2021/12/10.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
