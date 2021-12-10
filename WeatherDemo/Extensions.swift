7df20f8d9c5c941bee9ef962db39198e35a4ddc4
Liyou <liyouleo911@msn.com>
2021-12-10 15:11:32 +0800

    add weather manager and response body

---
+++ WeatherDemo/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherDemo
+//
+//  Created by Liyou on 2021/12/10.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
---
+++ WeatherDemo/Views/WeatherView.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherView.swift
+//  WeatherDemo
+//
+//  Created by Liyou on 2021/12/10.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    
+    var weather: ResponseBody
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
+}
