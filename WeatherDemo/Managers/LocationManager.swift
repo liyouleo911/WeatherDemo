80425cfe85ad4dbf2e9db7fae57a2efbc3b4f069
Liyou <liyouleo911@msn.com>
2021-12-10 17:45:37 +0800

    优化location manager

--- WeatherDemo/Managers/LocationManager.swift
+++ WeatherDemo/Managers/LocationManager.swift
@@ -10,32 +10,45 @@ import CoreLocation
 
 class LocationManager: NSObject, ObservableObject {
     
-    let manager = CLLocationManager()
+    private let locationManager: CLLocationManager = {
+        let locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        return locationManager
+    }()
     
     @Published var location: CLLocationCoordinate2D?
     @Published var isLoading = false
-    
-    override init() {
-        super.init()
-        manager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        manager.delegate = self
-    }
-    
+
     func requestLocation() {
-        isLoading = true
-        manager.requestLocation()
+        DispatchQueue.main.asyncAfter(deadline: .now()) { [self] in
+            locationManager.delegate = self
+            if locationManager.authorizationStatus != .notDetermined {
+                isLoading = true
+                locationManager.requestLocation()
+            }
+        }
     }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        debugPrint(#function)
+        if status != .notDetermined {
+            isLoading = true
+            manager.requestLocation()
+        }
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        debugPrint(#function)
         location = locations.first?.coordinate
         isLoading = false
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Error getting location", error)
+        debugPrint(#function)
+        debugPrint("Error getting location: \(String(describing: error))")
         isLoading = false
     }
 }
