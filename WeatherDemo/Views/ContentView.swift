74857a491b7bfd9f546b7d3de08499df1077cf1b
Liyou <liyouleo911@msn.com>
2021-12-13 17:14:08 +0800

    优化后台回来定位-请求天气时序

--- WeatherDemo/Views/ContentView.swift
+++ WeatherDemo/Views/ContentView.swift
@@ -12,32 +12,42 @@ struct ContentView: View {
     @StateObject var locationManager = LocationManager()
     var weatherManager = WeatherManager()
     @State var weather: ResponseBody?
-    @State var lastWeather: ResponseBody?
+    @State var firstWeather = true
     @Environment(\.scenePhase) var scenePhase
     var body: some View {
         VStack {
             if let location = locationManager.location {
-                if let weather = weather {
-                    WeatherView(weather: weather)
-                } else if let lastWeather = lastWeather {
-                    WeatherView(weather: lastWeather)
-                        .task {
-                            do {
-                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
-                            } catch {
-                                print("Error getting weather: \(error)")
-                            }
-                        }
+                if locationManager.isLoading {
+                    if let weather = weather {
+                        WeatherView(weather: weather)
+                    } else {
+                        ProgressView()
+                    }
                 } else {
-                    ProgressView()
-                        .task {
-                            do {
-                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
-                            } catch {
-                                print("Error getting weather: \(error)")
-                                locationManager.location = nil
+                    if let weather = weather {
+                        WeatherView(weather: weather)
+                            .task {
+                                guard !firstWeather else {
+                                    firstWeather = false
+                                    return
+                                }
+                                do {
+                                    self.weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                                } catch {
+                                    print("Error getting weather: \(error)")
+                                }
                             }
-                        }
+                    } else {
+                        ProgressView()
+                            .task {
+                                do {
+                                    weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                                } catch {
+                                    print("Error getting weather: \(error)")
+                                    locationManager.location = nil
+                                }
+                            }
+                    }
                 }
             } else {
                 if locationManager.isLoading {
@@ -55,8 +65,6 @@ struct ContentView: View {
                 return
             }
             if scenePhase == .background, newPhase == .inactive {
-                lastWeather = weather
-                weather = nil
                 locationManager.requestLocation()
             }
         }
